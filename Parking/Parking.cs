064997b5c68fce27f9c99abbeeb74de7bc76e54f
mrwnmncd <69033265+mrwnmncd@users.noreply.github.com>
2023-12-03 17:29:29 +0800

    construct vehicle

---
+++ Parking/Parking.cs
@@ -0,0 +1,23 @@
+public class Vehicle
+{
+    public string? Make { get; set; }
+    public string? Model { get; set; }
+    public string? PlateNumber { get; set; }
+
+    public static (string? make, string? model, string plateNumber) PromptUser()
+    {
+        Console.Write("Enter vehicle make (manufacturer) [enter to skip]: ");
+        string? make = Console.ReadLine();
+        Console.Write("Enter vehicle model [enter to skip]: ");
+        string? model = Console.ReadLine();
+        Console.Write("Enter vehicle plate number [required]: ");
+        string plateNumber = Console.ReadLine()!;
+        if (string.IsNullOrEmpty(plateNumber) || string.IsNullOrWhiteSpace(plateNumber))
+        {
+            Console.WriteLine("Plate number is required!"); // TODO: remove console log
+            // TODO: handle null
+            // TODO: loop until valid input
+        }
+        return (make, model, plateNumber);
+    }
+}
