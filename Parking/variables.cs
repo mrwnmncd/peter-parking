f2d7f4352543579ab4337616e3cedb9e566ee413
mrwnmncd <69033265+mrwnmncd@users.noreply.github.com>
2023-12-03 17:24:46 +0800

    configure variable file

---
+++ Parking/variables.cs
@@ -0,0 +1,9 @@
+class Environment
+{
+    public static string name = "Peter Parking";
+    public static int spaces = 50;
+    public static Dictionary<string, string> credentials = new Dictionary<string, string>() {
+        {"username", "admin"},
+        {"password", "admin"}
+    };
+}
