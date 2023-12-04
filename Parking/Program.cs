2b6a5095c3e2a9029b575750e75e6114bd3f96fd
mrwnmncd <69033265+mrwnmncd@users.noreply.github.com>
2023-12-04 16:39:41 +0800

    Create more features
    Update the menu:
    - Search Vehicle
    - Update Parking Details

--- Parking/Program.cs
+++ Parking/Program.cs
@@ -20,10 +20,12 @@ namespace PeterParking
                 Console.WriteLine(Environment.name + '\n');
                 Console.WriteLine("a. Add Parking Lot");
                 Console.WriteLine("b. Assign Parking Lot");
-                Console.WriteLine("c. Remove Parking Assignment");
-                Console.WriteLine("d. Show Parking Space");
-                Console.WriteLine("e. Find Nearest Parking Lot");
-                Console.WriteLine("f. Exit Application");
+                Console.WriteLine("c. Update Parking Details");
+                Console.WriteLine("d. Remove Parking Assignment");
+                Console.WriteLine("e. Show Parking Space");
+                Console.WriteLine("f. Search Vehicle [UNFINISHED]");
+                Console.WriteLine("g. Find Nearest Free Parking");
+                Console.WriteLine("h. Exit Application");
 
                 Console.WriteLine();
 
@@ -32,18 +34,18 @@ namespace PeterParking
                 choice = choice.ToLower();
 
                 Console.WriteLine('\n');
-                // TODO: car type
-                // TODO: update parking deetsd
                 if (choice == "a") { parking.ExtendParkingSpace(); continue; }
                 if (choice == "b") { parking.ParkVehicle(); continue; }
-                if (choice == "c") { parking.RemoveVehicle(); continue; }
-                if (choice == "d") { parking.ListVehicles(); continue; }
-                if (choice == "e") { parking.SearchNearestFreeParking(); continue; }
-                if (choice == "f") { Console.WriteLine('\n' + "Goodbye!"); break; }
+                if (choice == "c") { parking.UpdateParkingDetails(); continue; }
+                if (choice == "d") { parking.RemoveVehicle(); continue; }
+                if (choice == "e") { parking.ListVehicles(); continue; }
+                if (choice == "f") { continue; }
+                if (choice == "g") { parking.SearchNearestFreeParking(); continue; }
+                if (choice == "h") { Console.WriteLine('\n' + "Goodbye!"); break; }
                 else
                 {
                     Console.WriteLine("Invalid choice!");
-                    Console.WriteLine("Select from optiona A to E only!");
+                    Console.WriteLine("Select from optiona A to H only!");
                     Console.WriteLine('\n');
                     continue;
                 }
